c4d72bb1dd02ad2602f9bbef17bf80824d843ce0
furkan tekkol <tekkol.furkan@gmail.com>
2021-10-22 17:53:54 +0300

    18 Ekim Ders Sonu

--- src/mapFilterReduce/index.js
+++ src/mapFilterReduce/index.js
@@ -33,7 +33,7 @@ console.log(cardTotal)
 //-----------------------map-filter-reduce------------------------------------
 
 let cardTotal2 = products.filter(p=>p.unitPrice>13000)      // Gelen datatı fiyatı 13000 den yukarı olacak şekilde filtrelendi.
-    .map(p=>{p.unitPrice = p.unitPrice*1.18; return p})     // Filtrelenen herbir ürün için önerek KDV hesaplandı.
+    .map(p=>{p.unitPrice = p.unitPrice*1.18; return p})     // Filtrelenen herbir ürün için dönerek KDV hesaplandı.
     .reduce((acc, p)=>acc + p.unitPrice, 0)                 // Fiyat ekleme uygulandı. (acc)
 
 console.log(cardTotal2)
